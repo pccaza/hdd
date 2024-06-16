d5a4d5009f0c7eb18dcc62d4f1b3195ea49d5780
pc <pcaz1701@gmail.com>
2024-06-16 14:48:01 -0500

    change token

--- app/page.tsx
+++ app/page.tsx
@@ -17,12 +17,12 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="flex flex-col gap-16 items-center">
         <div className="text-3xl font-extrabold tracking-tight underline text-green-500">
-          🚀 $HDD ROAD TO $10MIL
+          🚀 $tilin ROAD TO $10MIL
         </div>
         <div className="z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
           <img
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-            src="/hdd.jpg"
+            src="/tilin.webp"
             alt="daddy tate"
             width={500}
             height={500}
@@ -30,9 +30,9 @@ export default function Home() {
         </div>
         <div>
           <div className="flex items-center gap-4">
-            <span className="text-lg font-bold">$HDD TOKEN ADDRESS://</span>
+            <span className="text-lg font-bold">$tilin TOKEN ADDRESS://</span>
             <div className="px-4 flex gap-2 py-2 rounded-lg bg-gray-700">
-              <span>7A9xBYGYFBG2zsiin2rhaKQ147Ejz3B4Sam4dJdpump</span>
+              <span>HW3s4qTcVBDgGcksr4Z5wgV8m6MaQt5EevMFBdPzpump</span>
               <Clipboard
                 className="hover:scale-2 hover:cursor-pointer"
                 onClick={() => copyText()}
@@ -45,13 +45,13 @@ export default function Home() {
       <div className="py-16">
         <div className="text-2xl font-extrabold">Follow the PUMP $$$</div>
         <div className="grid grid-cols-2 py-4">
-          <Link href={"https://t.me/+uiAuPzuiB245YTMx"}>
+          <Link href={"https://t.me/+aH51zy-qkfcxZDhl"}>
             <div className="h-12 w-12">
               <TelegramIcon></TelegramIcon>
             </div>
             <div className="text-lg font-bold">Telegram</div>
           </Link>
-          <Link href={"https://x.com/ddcoin1111"}>
+          <Link href={"https://x.com/tilin_cto"}>
             <div className="h-12 w-12">
               <TwitterIcon></TwitterIcon>
             </div>
